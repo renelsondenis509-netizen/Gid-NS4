978b43ef0406a1c1bb1290a2f345aa1466208233
Renelson Denis <renelsondenis509@gmail.com>
2026-04-16 15:19:22 -0500

    Ajout du fichier de configuration src/config.js

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const API            = `${SUPABASE_URL}/functions/v1/ask-prof-lakay`;
+export const APP_LOGO       = "/logo.png?v=2";
+export const PROF_LAKAY_PHOTO = "https://i.postimg.cc/MH8V3LKv/Jpgpro-out-bfea41d353cc11eb57c8fc16e3b40ffa.jpg";
