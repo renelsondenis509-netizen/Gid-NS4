03381908854219c7991cdba63eb2ed1246a96a31
Renelson Denis <renelsondenis509@gmail.com>
2026-07-22 20:20:39 -0500

    Retire iconColor: laisse Android gérer le contraste par défaut de l'icône de notification

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -37,7 +37,6 @@ export async function scheduleDailyReminder(hour = 18) {
         schedule: { on: { hour, minute: 0 }, repeats: true, allowWhileIdle: true },
         sound: null,
         smallIcon: "ic_stat_notify",
-        iconColor: "#E8002A",
         channelId: CHANNEL_ID,
       }]
     });
@@ -57,7 +56,6 @@ export async function scheduleExpiryReminder(daysRemaining) {
         schedule: { at: new Date(Date.now() + 1000) },
         sound: null,
         smallIcon: "ic_stat_notify",
-        iconColor: "#E8002A",
         channelId: CHANNEL_ID,
       }]
     });
